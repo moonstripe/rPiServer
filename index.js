3ec0ac1112e858c7a1ef4e55e76a5ad1231fcf9e
moonstripe <moonstripe@protonmail.com>
2021-02-14 20:29:41 -0800

    i'm stupid

--- index.js
+++ index.js
@@ -12,28 +12,6 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(routes);
-app.get('/', (req, res) => {
-  // console.log(req.rawTrailers);
-
-  // Do python script
-  let dataToSend;
-  // spawn new child process to call the python script
-  const python = spawn('python', ['./python/test.py']);
-  // collect data from script
-  python.stdout.on('data', function (data) {
-    console.log('Pipe data from python script ...');
-    dataToSend = data.toString();
-  });
-  // in close event we are sure that stream from child process is closed
-  python.on('close', (code) => {
-    console.log(`child process close all stdio with code ${code}`);
-    // send data to browser
-    res.send('do actual python v0.1')
-  });
-
-
-  res.send('do python script and more stuff')
-});
 
 app.listen(3001, () => {
   console.log('Server started listening on PORT http://localhost:3001');
