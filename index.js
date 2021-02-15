94fde5931444da5119d4710a9dffca5c2bafbd90
moonstripe <moonstripe@protonmail.com>
2021-02-14 20:25:33 -0800

    forgot spawn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const {spawn} = require('child_process');
 const routes = require('./routes');
 
 const app = express();
