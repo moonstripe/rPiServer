e63c7cd4b519d2a66a0983e3a9832ac14c3970ea
moonstripe <moonstripe@protonmail.com>
2021-02-14 20:24:26 -0800

    server python

--- index.js
+++ index.js
@@ -12,12 +12,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(routes);
 app.get('/', (req, res) => {
-    console.log(req.rawTrailers);
+  // console.log(req.rawTrailers);
 
-// Do python script
+  // Do python script
+  let dataToSend;
+  // spawn new child process to call the python script
+  const python = spawn('python', ['./python/test.py']);
+  // collect data from script
+  python.stdout.on('data', function (data) {
+    console.log('Pipe data from python script ...');
+    dataToSend = data.toString();
+  });
+  // in close event we are sure that stream from child process is closed
+  python.on('close', (code) => {
+    console.log(`child process close all stdio with code ${code}`);
+    // send data to browser
+    res.send(dataToSend)
+  });
 
 
-    res.send('do python script and more stuff')
+  res.send('do python script and more stuff')
 });
 
 app.listen(3001, () => {
